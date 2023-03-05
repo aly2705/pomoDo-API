7b29917679b614539c108cccca539fbdaa850104
aly2705 <alexandraaly.simion@gmail.com>
2023-03-05 11:10:58 +0200

    added messages for operational errors in production

--- controllers/errorController.ts
+++ controllers/errorController.ts
@@ -1,7 +1,39 @@
 import AppError from '../utilities/AppError';
 import { NextFunction, Request, Response } from 'express';
+import { Error, MongooseError } from 'mongoose';
+import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
+import { MongoServerError } from 'mongodb';
 
 // #ff0000: Complete with specific messages for operational errors
+
+// JWT Errors
+const handleJWTExpired = () => {
+  const message = 'Your token has expired! Please log in again!';
+  return new AppError(message, 401);
+};
+const handleJWTInvalid = () => {
+  const message = `Invalid token! Please log in again!`;
+  return new AppError(message, 401);
+};
+
+// DB Errors
+const handleCastErrorDB = (err: Error.CastError) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+const handleDuplicateFieldDB = (err: MongoServerError) => {
+  console.log(err);
+
+  const value = err.errmsg.match(/(["'])(?:\\.|[^\\])*?\1/)?.at(0) || '';
+  const message = `Duplicate field value: ${value} . Please use another value`;
+
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = (err: Error.ValidationError) => {
+  const errors = Object.values(err.errors).map(value => value.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
 
 const sendErrorProd = (err: AppError, res: Response): void => {
   if (err.isOperational) {
@@ -29,20 +61,34 @@ const sendErrorDev = (err: AppError, res: Response): void => {
 };
 
 const globalErrorHandler = (
-  caughtError: Error | AppError,
+  caughtError: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   let error: AppError;
 
-  if (!(caughtError instanceof AppError)) {
-    error = new AppError(caughtError.message, 500, false);
-  } else error = caughtError;
-
   if (process.env.NODE_ENV === 'development') {
+    if (!(caughtError instanceof AppError)) {
+      error = new AppError(caughtError.message, 500, false);
+    } else error = caughtError;
     sendErrorDev(error, res);
   } else {
+    error = Object.create(caughtError);
+
+    if (caughtError instanceof Error.CastError)
+      error = handleCastErrorDB(caughtError);
+
+    if (caughtError instanceof MongoServerError && caughtError.code === 11000)
+      error = handleDuplicateFieldDB(caughtError);
+
+    if (caughtError instanceof Error.ValidationError)
+      error = handleValidationErrorDB(caughtError);
+
+    if (caughtError instanceof JsonWebTokenError) error = handleJWTInvalid();
+
+    if (caughtError instanceof TokenExpiredError) error = handleJWTExpired();
+
     sendErrorProd(error, res);
   }
 };
